322a4f18b1d76c2763da22da26d80ce6ac83fcb3
Aravella Lousta <73483257+aravellalousta@users.noreply.github.com>
2023-10-11 23:37:14 +0300

    gui progress for direct mapped
    new plan for gui with tables for cache, ram and addresses to check, hit/miss indicator and results shown as labels

--- cache/src/cache/GUI.java
+++ cache/src/cache/GUI.java
@@ -1,14 +1,15 @@
 package cache;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GUI extends JPanel {
-    Font customFont = new Font("Arial", Font.BOLD, 14);
+    Font customFont = new Font("Arial", Font.BOLD, 16);
     private JPanel directPanel;
     private JPanel fullyPanel;
     private JPanel setPanel;
@@ -20,7 +21,7 @@ public class GUI extends JPanel {
     public void frameConfiguration() {
         JFrame frame = new JFrame("Cache Simulation");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 600);
+        frame.setSize(1100, 500);
 
         // Create the tabbed pane
         JTabbedPane tabbedPane = tabsConfiguration();
@@ -37,10 +38,9 @@ public class GUI extends JPanel {
 
         // Create content panels for each tab
         directPanel = new JPanel(new GridLayout(1, 2));
-        fullyPanel = new JPanel();
-        setPanel = new JPanel();
+        fullyPanel = new JPanel(new GridLayout(1, 2));
+        setPanel = new JPanel(new GridLayout(1, 2));
 
-        // Add the content panels to the JTabbedPane with tab titles
         tabbedPane.addTab("Direct Mapped Cache", directPanel);
         tabbedPane.addTab("Fully Associative Cache", fullyPanel);
         tabbedPane.addTab("Set Associative Cache", setPanel);
@@ -50,16 +50,18 @@ public class GUI extends JPanel {
 
         // Link the tabs to their respective methods
         for (int i = 0; i < 3; i++) {
-            if (i == 0) { // Direct Mapped Cache tab is selected
+            if (i == 0) {
                 displaySelectedTabContents(directPanel);
-            } else if (i == 1) { // Fully Associative Cache tab is selected
-                performFullyAssociativeCacheAction();
-            } else if (i == 2) { // Set Associative Cache tab is selected
-                performSetAssociativeCacheAction();
+            } else if (i == 1) {
+                displaySelectedTabContents(fullyPanel);
+            } else if (i == 2) {
+
+                // TO CHANGE : need to change this in order to add the k-ways option
+                displaySelectedTabContents(setPanel);
             }
         }
 
-        return tabbedPane; // Return the JTabbedPane
+        return tabbedPane;
     }
 
     public void stylingTabs(JTabbedPane tabbedPane) {
@@ -67,55 +69,162 @@ public class GUI extends JPanel {
 
         tabbedPane.setFont(customFont);
 
-        // Add a ChangeListener to the JTabbedPane to change the color of the selected
-        // tab
         tabbedPane.addChangeListener(e -> {
             Component selectedComponent = tabbedPane.getSelectedComponent();
             for (int i = 0; i < tabbedPane.getTabCount(); i++) {
                 if (tabbedPane.getComponentAt(i) == selectedComponent) {
-                    // Set the background color of the selected tab's content panel
                     tabbedPane.setBackgroundAt(i, selectedTabColor);
-                    // Perform specific tab actions here, if needed
                 } else {
-                    // Reset the background color of other tabs' content panels
                     tabbedPane.setBackgroundAt(i, null);
                 }
             }
         });
 
-        // Set the initial selected tab's background color
         tabbedPane.setBackgroundAt(tabbedPane.getSelectedIndex(), selectedTabColor);
     }
 
     // Add methods for functionality of each tab
     public void displaySelectedTabContents(JPanel panel) {
-
-        panel.setBackground(Color.pink);
-
         // Left column: Add a nested JPanel with GridLayout for 3 input fields
         JPanel leftPanel = new JPanel(new GridBagLayout());
+        leftPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        configureLeftPanel(panel, leftPanel);
 
-        // Create GridBagConstraints for left column
-        GridBagConstraints leftConstraints = new GridBagConstraints();
-        leftConstraints.anchor = GridBagConstraints.WEST;
-        leftConstraints.insets = new Insets(5, 5, 5, 5);
+        JPanel rightPanel = new JPanel(new GridBagLayout());
+        configureRightPanel(rightPanel);
 
+        // Add the left and right panels to the main panel
+        panel.add(leftPanel);
+        panel.add(rightPanel);
+
+    }
+
+    private void configureRightPanel(JPanel rightPanel) {
         // Create GridBagConstraints for right column
         GridBagConstraints rightConstraints = new GridBagConstraints();
         rightConstraints.anchor = GridBagConstraints.WEST;
         rightConstraints.insets = new Insets(5, 5, 5, 5);
 
-        JLabel leftColumnLabel = new JLabel("Inputs");
+        JLabel rightColumnLabel = new JLabel("Cache Mapping");
+        rightColumnLabel.setFont(customFont);
+
+        rightConstraints.gridx = 0;
+        rightConstraints.gridy = 0;
+        rightPanel.add(rightColumnLabel, rightConstraints);
+
+        // Cache Table
+        JLabel cacheTableLabel = new JLabel("Current State of Cache");
+        rightConstraints.gridx = 0;
+        rightConstraints.gridy = 1;
+        rightPanel.add(cacheTableLabel, rightConstraints);
+
+        DefaultTableModel modelCache = new DefaultTableModel();
+        modelCache.addColumn("Tag");
+        modelCache.addColumn("Line");
+        modelCache.addColumn("Offset");
+
+        JTable cacheTable = new JTable(modelCache);
+
+        JScrollPane scrollPane = new JScrollPane(cacheTable);
+        scrollPane.setPreferredSize(new Dimension(200, 100));
+
+        rightConstraints.gridx = 0;
+        rightConstraints.gridy = 2;
+        rightPanel.add(scrollPane, rightConstraints);
+
+        // Ram Table
+        JLabel ramTableLabel = new JLabel("Current State of RAM");
+        rightConstraints.gridx = 1;
+        rightConstraints.gridy = 1;
+        rightPanel.add(ramTableLabel, rightConstraints);
+
+        DefaultTableModel modelRam = new DefaultTableModel();
+        modelRam.addColumn("Tag");
+        modelRam.addColumn("Data");
+        JTable ramTable = new JTable(modelRam);
+
+        JScrollPane scrollPaneRam = new JScrollPane(ramTable);
+        scrollPaneRam.setPreferredSize(new Dimension(200, 100));
+
+        rightConstraints.gridx = 1;
+        rightConstraints.gridy = 2;
+        rightPanel.add(scrollPaneRam, rightConstraints);
+
+        // Input for searching in the Cache
+        // TO CHANGE
+        JLabel testingAddressLabel = new JLabel("Testing Address:");
+        testingAddressLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0)); // Set top padding
+
+        JLabel testingAddress = new JLabel("000000000");
+        testingAddress.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0)); // Set top padding
+
+        rightConstraints.gridx = 0;
+        rightConstraints.gridy = 3;
+        rightPanel.add(testingAddressLabel, rightConstraints);
+
+        rightConstraints.gridx = 1;
+        rightConstraints.gridy = 3;
+        rightPanel.add(testingAddress, rightConstraints);
+
+        // Area highlighting hit or miss
+        JPanel indicatorPanel = new JPanel();
+        indicatorPanel.setLayout(new GridBagLayout());
+        indicatorPanel.setBackground(Color.green);
+
+        JLabel label = new JLabel("Hit");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        indicatorPanel.add(label);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0; // Start at column 0
+        gbc.gridy = 4; // Start at row 0
+        gbc.gridwidth = GridBagConstraints.REMAINDER; // Span the entire row
+        gbc.fill = GridBagConstraints.BOTH;
+        rightPanel.add(indicatorPanel, gbc);
+
+        // Section for stats
+        JLabel statsLabel = new JLabel("Results");
+        statsLabel.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0)); // Set top padding
+        statsLabel.setFont(customFont);
+
+        rightConstraints.gridx = 0;
+        rightConstraints.gridy = 5;
+        rightPanel.add(statsLabel, rightConstraints);
+
+        JLabel missRateLabel = new JLabel("Miss Rate");
+        rightConstraints.gridx = 0;
+        rightConstraints.gridy = 6;
+        rightPanel.add(missRateLabel, rightConstraints);
+
+        JLabel missRate = new JLabel("0.0%");
+        rightConstraints.gridx = 1;
+        rightConstraints.gridy = 6;
+        rightPanel.add(missRate, rightConstraints);
+
+    }
+
+    private void configureLeftPanel(JPanel panel, JPanel leftPanel) {
+        // Create GridBagConstraints for left column
+        GridBagConstraints leftConstraints = new GridBagConstraints();
+        leftConstraints.anchor = GridBagConstraints.WEST;
+        leftConstraints.insets = new Insets(5, 5, 5, 5);
+
+        JLabel leftColumnLabel = new JLabel("Configuration");
+        leftColumnLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0)); // Set top padding
+
         leftColumnLabel.setFont(customFont);
 
-        JLabel ramSizeInputLabel = new JLabel("Enter RAM Size:");
-        JTextField ramSizeInput = new JTextField(5);
+        JLabel ramSizeInputLabel = new JLabel("Choose RAM Size");
+        JRadioButton ramSizeOption1 = new JRadioButton("128 bytes");
+        JRadioButton ramSizeOption2 = new JRadioButton("256 bytes");
 
-        JLabel cacheSizeInputLabel = new JLabel("Enter Cache Size:");
-        JTextField cacheSizeInput = new JTextField(5);
+        JLabel cacheSizeInputLabel = new JLabel("Choose Cache Size");
+        JRadioButton cacheSizeOption1 = new JRadioButton("16 bytes");
+        JRadioButton cacheSizeOption2 = new JRadioButton("32 bytes");
 
-        JLabel blockSizeInputLabel = new JLabel("Enter Block Size:");
-        JTextField blockSizeInput = new JTextField(5);
+        JLabel blockSizeInputLabel = new JLabel("Choose Block Size:");
+        JRadioButton blockSizeOption = new JRadioButton("4 words");
 
         JButton submitBtn = new JButton("Submit");
 
@@ -137,37 +246,34 @@ public class GUI extends JPanel {
 
         leftConstraints.gridx = 1;
         leftConstraints.gridy = 1;
-        leftPanel.add(ramSizeInput, leftConstraints);
+        leftPanel.add(ramSizeOption1, leftConstraints);
+
+        leftConstraints.gridx = 2;
+        leftConstraints.gridy = 1;
+        leftPanel.add(ramSizeOption2, leftConstraints);
 
         leftConstraints.gridx = 1;
         leftConstraints.gridy = 2;
-        leftPanel.add(cacheSizeInput, leftConstraints);
+        leftPanel.add(cacheSizeOption1, leftConstraints);
+
+        leftConstraints.gridx = 2;
+        leftConstraints.gridy = 2;
+        leftPanel.add(cacheSizeOption2, leftConstraints);
 
         leftConstraints.gridx = 1;
         leftConstraints.gridy = 3;
-        leftPanel.add(blockSizeInput, leftConstraints);
+        leftPanel.add(blockSizeOption, leftConstraints);
 
         leftConstraints.gridx = 1;
         leftConstraints.gridy = 4;
         leftPanel.add(submitBtn, leftConstraints);
-
-        // Right column: Add a progress bar
-        JProgressBar progressBar = new JProgressBar(0, 100);
-        progressBar.setValue(50);
-
-        // Add the left and right panels to the main panel
-        panel.add(leftPanel);
-        panel.add(progressBar);
-
     }
 
     public void performFullyAssociativeCacheAction() {
-        // Add your Fully Associative Cache functionality here
         System.out.println("Fully Associative Cache Action Performed");
     }
 
     public void performSetAssociativeCacheAction() {
-        // Add your Set Associative Cache functionality here
         System.out.println("Set Associative Cache Action Performed");
     }
 
